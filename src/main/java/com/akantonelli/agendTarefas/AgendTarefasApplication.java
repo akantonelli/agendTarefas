9ab925cd2510ec8d23788c4084f4fd945613e5d9
Andre <akantonelli99@gmail.com>
2026-03-05 20:29:00 -0300

    commit inicial

---
+++ src/main/java/com/akantonelli/agendTarefas/AgendTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.akantonelli.agendTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akantonelli/agendTarefas/AgendTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akantonelli.agendTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
