22b0c1ab7c5bf9c11c5ab70f3110c96a1d877294
Andre <akantonelli99@gmail.com>
2026-03-11 19:38:32 -0300

    Add lombok annotation in repositories

--- src/main/java/com/akantonelli/agendTarefas/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/akantonelli/agendTarefas/infrastructure/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package com.akantonelli.agendTarefas.infrastructure.repository;
 
 import com.akantonelli.agendTarefas.infrastructure.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 }
